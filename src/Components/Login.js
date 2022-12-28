def3c85b515a58595acf2302db94975d4654b5b8
shakil <mdshakil2898@gmail.com>
2022-12-28 16:11:08 +0600

    login & register implemented

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Context/UserContext';
 
 const Login = () => {
+   const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.from?.pathname || '/';
+  const {loginWithGoogle, signIn} = useContext(AuthContext);
+
+
+  const handleLogin =(event) =>{
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+    .then(result=>{
+      const user = result.user;
+      if(user.email){
+        navigate(from, {replace:true})
+      }
+    })
+    .catch(error=>{
+      
+    })
+    
+  }
+
+  const googleSignIn = () =>{
+    loginWithGoogle()
+    .then(result => {
+      navigate(from, {replace: true})
+    })
+    .catch(err => console.log(err))
+  }  
    return (
       
-<form>
+<form onSubmit={handleLogin}>
   <div class="mb-6">
     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
     <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required/>
@@ -18,7 +51,7 @@ const Login = () => {
     <Link to='/register' for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Register Now</Link>
   </div>
   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-5">Google Login</button>
+  <button onClick={googleSignIn} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-5">Google Login</button>
   
 </form>
 
